3e7140529ed07392c310b2599d636960e93e6369
Michael Jung <jung.michaelh@gmail.com>
2022-11-14 16:57:15 -0800

    Create template files

---
+++ easy.js
@@ -0,0 +1,25 @@
+// ---------- 1. TWO NUMBER SUM ---------- //
+// ---------- 2. VALIDATE SUBSEQUENCE ---------- //
+// ---------- 3. SORTED SQUARED ARRAY ---------- //
+// ---------- 4. TOURNAMENT WINNER ---------- //
+// ---------- 5. NON-CONSTRUCTIBLE CHANGE ---------- //
+// ---------- 6. FIND CLOSEST VALUE IN BST ---------- //
+// ---------- 7. BRANCH SUMS ---------- //
+// ---------- 8. NODE DEPTHS ---------- //
+// ---------- 9. DEPTH-FIRST SEARCH ---------- //
+// ---------- 10. MINIUMUM WAITING TIME ---------- //
+// ---------- 11. CLASS PHOTOS ---------- //
+// ---------- 12. TANDEM BICYCLE ---------- //
+// ---------- 13. REMOVE DUPLICATES FROM LINKED LISTS ---------- //
+// ---------- 14. NTH FIBONACCI ---------- //
+// ---------- 15. PRODUCT SUM ---------- //
+// ---------- 16. BINARY SEARCH ---------- //
+// ---------- 17. FIND THREE LARGEST NUMBERS ---------- //
+// ---------- 18. BUBBLE SORT ---------- //
+// ---------- 19. INSERTION SORT ---------- //
+// ---------- 20. SELECTION SORT ---------- //
+// ---------- 21. PALINDROME CHECK ---------- //
+// ---------- 22. CAESAR CIPHER ENCRYPTOR ---------- //
+// ---------- 23. RUN-LENGTH ENCODING ---------- //
+// ---------- 24. GENERATE DOCUMENT ---------- //
+// ---------- 25. FIRST NON-REPEATING CHARACTER ---------- //
