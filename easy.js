d96818ca6346adb2d30ce5cf03b53d12becbfb59
Michael Jung <jung.michaelh@gmail.com>
2022-11-20 09:48:32 -0800

    Add alternative solution for Binary Search

--- easy.js
+++ easy.js
@@ -409,6 +409,19 @@ function binarySearch(array, target, startIdx=0) {
         else return binarySearch(array.slice(0, m), target, startIdx);
     }
 }
+// // ALTERNATIVE SOLUTION:
+// function binarySearch(array, target) {
+//     let left = 0;
+//     let right = array.length - 1;
+//     while (left <= right) {
+//         const mid = Math.floor((left + right) / 2);
+//         if (array[mid] === target) return mid;
+//         else if (array[mid] < target) left = mid + 1;
+//         else right = mid - 1;
+//     }
+
+//     return -1;
+// }
 
 
 // ---------- 17. FIND THREE LARGEST NUMBERS ---------- //
