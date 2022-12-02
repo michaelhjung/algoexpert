ee71ddb8e11ac5fcb4dfcfe55e658f0283879cc2
Michael Jung <jung.michaelh@gmail.com>
2022-12-02 10:02:18 -0800

    Solve Smallest Difference and Move Element to End

--- medium.js
+++ medium.js
@@ -1,55 +1,101 @@
-// ---------- 1. # ---------- //
-// ---------- 2. # ---------- //
-// ---------- 3. # ---------- //
-// ---------- 4. # ---------- //
-// ---------- 5. # ---------- //
-// ---------- 6. # ---------- //
-// ---------- 7. # ---------- //
-// ---------- 8. # ---------- //
-// ---------- 9. # ---------- //
-// ---------- 10. # ---------- //
-// ---------- 11. # ---------- //
-// ---------- 12. # ---------- //
-// ---------- 13. # ---------- //
-// ---------- 14. # ---------- //
-// ---------- 15. # ---------- //
-// ---------- 16. # ---------- //
-// ---------- 17. # ---------- //
-// ---------- 18. # ---------- //
-// ---------- 19. # ---------- //
-// ---------- 20. # ---------- //
-// ---------- 21. # ---------- //
-// ---------- 22. # ---------- //
-// ---------- 23. # ---------- //
-// ---------- 24. # ---------- //
-// ---------- 25. # ---------- //
-// ---------- 26. # ---------- //
-// ---------- 27. # ---------- //
-// ---------- 28. # ---------- //
-// ---------- 29. # ---------- //
-// ---------- 30. # ---------- //
-// ---------- 31. # ---------- //
-// ---------- 32. # ---------- //
-// ---------- 33. # ---------- //
-// ---------- 34. # ---------- //
-// ---------- 35. # ---------- //
-// ---------- 36. # ---------- //
-// ---------- 37. # ---------- //
-// ---------- 38. # ---------- //
-// ---------- 39. # ---------- //
-// ---------- 40. # ---------- //
-// ---------- 41. # ---------- //
-// ---------- 42. # ---------- //
-// ---------- 43. # ---------- //
-// ---------- 44. # ---------- //
-// ---------- 45. # ---------- //
-// ---------- 46. # ---------- //
-// ---------- 47. # ---------- //
-// ---------- 48. # ---------- //
-// ---------- 49. # ---------- //
-// ---------- 50. # ---------- //
-// ---------- 51. # ---------- //
-// ---------- 52. # ---------- //
-// ---------- 53. # ---------- //
-// ---------- 54. # ---------- //
-// ---------- 55. # ---------- //
+// ---------- 1. THREE NUMBER SUM ---------- //
+// ---------- 2. SMALLEST DIFFERENCE ---------- //
+// //BRUTE FORCE METHOD:
+// function smallestDifference(arrayOne, arrayTwo) {
+//     const smallestDistance = {
+//         v1: arrayOne[0],
+//         v2: arrayTwo[0],
+//         distance: Math.abs(arrayOne[0] - arrayTwo[0])
+//     }
+
+//     for (let i = 0; i < arrayOne.length; i++) {
+//         for (let j = 0; j < arrayTwo.length; j++) {
+//             if (Math.abs(arrayOne[i] - arrayTwo[j]) < smallestDistance.distance) {
+//                 smallestDistance.v1 = arrayOne[i];
+//                 smallestDistance.v2 = arrayTwo[j];
+//                 smallestDistance.distance = Math.abs(arrayOne[i] - arrayTwo[j]);
+//             }
+//         }
+//     }
+
+//     return [smallestDistance.v1, smallestDistance.v2];
+// }
+function smallestDifference(arrayOne, arrayTwo) {
+
+}
+// ---------- 3. MOVE ELEMENT TO END ---------- //
+// // BRUTE FORCE METHOD:
+// function moveElementToEnd(array, toMove) {
+//     array.sort();
+//     let startSpliceI;
+//     let numToSplice = 1;
+//     for (let i = 0; i < array.length; i++) {
+//         if ((startSpliceI || startSpliceI === 0) && array[i] === toMove) numToSplice++;
+//         else if ((!startSpliceI && startSpliceI !== 0) && array[i] === toMove) startSpliceI = i;
+//         else if ((startSpliceI || startSpliceI === 0) && array[i] !== toMove) {
+//             const spliced = array.splice(startSpliceI, numToSplice);
+//             array.push(...spliced);
+//             break;
+//         }
+//     }
+
+//     return array;
+// }
+function moveElementToEnd(array, toMove) {
+
+}
+// ---------- 4. MONOTONIC ARRAY ---------- //
+// ---------- 5. SPIRAL TRAVERSE ---------- //
+// ---------- 6. LONGEST PEAK ---------- //
+// ---------- 7. ARRAY OF PRODUCTS ---------- //
+// ---------- 8. FIRST DUPLICATE VALUE ---------- //
+// ---------- 9. MERGE OVERLAPPING INTERVALS ---------- //
+// ---------- 10. BST CONSTRUCTION ---------- //
+// ---------- 11. VALIDATE BST ---------- //
+// ---------- 12. BST TRAVERSAL ---------- //
+// ---------- 13. MIN HEIGHT BST ---------- //
+// ---------- 14. FIND KTH LARGEST VALUE IN BST ---------- //
+// ---------- 15. RECONSTRUCT BST ---------- //
+// ---------- 16. INVERT BINARY TREE ---------- //
+// ---------- 17. BINARY TREE DIAMETER ---------- //
+// ---------- 18. FIND SUCCESSOR ---------- //
+// ---------- 19. HEIGHT BALANCED BINARY TREE ---------- //
+// ---------- 20. MAX SUBSET SUM NO ADJACENT ---------- //
+// ---------- 21. NUMBER OF WAYS TO MAKE CHANGE ---------- //
+// ---------- 22. MIN NUMBER OF COINS FOR CHANGE ---------- //
+function minNumberOfCoinsForChange(n, denoms) {
+    // Write your code here.
+}
+// ---------- 23. LEVENSHTEIN DISTANCE ---------- //
+// ---------- 24. NUMBER OF WAYS TO TRAVERSE GRAPH ---------- //
+// ---------- 25. KADANE'S ALGORITHM ---------- //
+// ---------- 26. SINGLE CYCLE CHECK ---------- //
+// ---------- 27. BREADTH-FIRST SEARCH ---------- //
+// ---------- 28. RIVER SIZES ---------- //
+// ---------- 29. YOUNGEST COMMON ANCESTOR ---------- //
+// ---------- 30. REMOVE ISLANDS ---------- //
+// ---------- 31. CYCLE IN GRAPH ---------- //
+// ---------- 32. MINIMUM PASSES OF MATRIX ---------- //
+// ---------- 33. TASK ASSIGNMENT ---------- //
+// ---------- 34. VALID STARTING CITY ---------- //
+// ---------- 35. MIN HEAP CONSTRUCTION ---------- //
+// ---------- 36. LINKED LIST CONSTRUCTION ---------- //
+// ---------- 37. REMOVE KTH NODE FROM END ---------- //
+// ---------- 38. SUM OF LINKED LISTS ---------- //
+// ---------- 39. PERMUTATIONS ---------- //
+// ---------- 40. POWERSET ---------- //
+// ---------- 41. PHONE NUMBER MNEMONICS ---------- //
+// ---------- 42. STAIRCASE TRAVERSAL ---------- //
+// ---------- 43. SEARCH IN SORTED MATRIX ---------- //
+// ---------- 44. THREE NUMBER SORT ---------- //
+// ---------- 45. MIN MAX STACK CONSTRUCTION ---------- //
+// ---------- 46. BALANCED BRACKETS ---------- //
+// ---------- 47. SUNSET VIEWS ---------- //
+// ---------- 48. SORT STACK ---------- //
+// ---------- 49. NEXT GREATER ELEMENT ---------- //
+// ---------- 50. LONGEST PALINDROMIC SUBSTRING ---------- //
+// ---------- 51. GROUP ANAGRAMS ---------- //
+// ---------- 52. VALID IP ADDRESSES ---------- //
+// ---------- 53. REVERSE WORDS IN STRING ---------- //
+// ---------- 54. MINIMUM CHARACTERS FOR WORDS ---------- //
+// ---------- 55. SUFFIX TRIE CONSTRUCTION ---------- //
