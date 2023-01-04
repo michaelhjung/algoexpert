d96766ff24e7d8d6aa0245ed862c7bf00f73440a
Michael Jung <jung.michaelh@gmail.com>
2023-01-03 16:59:17 -0800

    Update Remove Islands solution

--- medium.js
+++ medium.js
@@ -463,14 +463,9 @@ function removeIslands(matrix) {
         }
     }
 
-    for (let row = 1; row < matrix.length - 1; row++) {
-        for (let col = 1; col < matrix[row].length - 1; col++) {
-            if (matrix[row][col] === 1) matrix[row][col] = 0;
-        }
-    }
-
     for (let row = 0; row < matrix.length; row++) {
         for (let col = 0; col < matrix[row].length; col++) {
+            if (matrix[row][col] === 1) matrix[row][col] = 0;
             if (matrix[row][col] === 2) matrix[row][col] = 1;
         }
     }
