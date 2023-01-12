d3b62bcc7308d232e12c05274946e656d017dee6
Michael Jung <jung.michaelh@gmail.com>
2023-01-12 13:50:37 -0800

    Solve Merge Sort

--- very-hard.js
+++ very-hard.js
@@ -29,6 +29,33 @@
 // ---------- 29. Number of Binary Tree Topologies ---------- //
 // ---------- 30. Non-Attacking Queens ---------- //
 // ---------- 31. Merge Sort ---------- //
+function mergeSort(array) {
+    // Write your code here.
+    if (array.length === 1) return array;
+
+    const mid = Math.floor(array.length / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
+
+    return mergeSortedArrays(mergeSort(left), mergeSort(right));
+}
+//helper:
+function mergeSortedArrays(left, right) {
+    const sortedArray = [];
+    let i = 0, j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            sortedArray.push(left[i]);
+            i++;
+        } else {
+            sortedArray.push(right[j]);
+            j++;
+        }
+    }
+    if (i < left.length) sortedArray.push(...left.slice(i));
+    if (j < right.length) sortedArray.push(...right.slice(j));
+    return sortedArray;
+}
 // ---------- 32. Count Inversions ---------- //
 // ---------- 33. Smallest Substring Containing ---------- //
 // ---------- 34. Longest Balanced Substring ---------- //
